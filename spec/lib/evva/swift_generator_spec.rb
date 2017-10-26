1efaa48234fe9143139ffa2bbc14de32a105c1cf
RicardoTrindade <ricardo.trindade743@gmail.com>
2017-10-26 17:44:42 +0100

    improves specs

--- spec/lib/evva/swift_generator_spec.rb
+++ spec/lib/evva/swift_generator_spec.rb
@@ -20,45 +20,28 @@ describe Evva::SwiftGenerator do
         class MixpanelHelper: NSObject {
           enum Event {
             case trackNavFeedTap
-            case trackCpPageView(course_id:Long,course_name:String?)
+            case trackCpPageView(course_id: Long, course_name: String?)
           }
-        private var data: EventData {
-          switch self {
 
-          case .trackNavFeedTap
-        return EventData(name:"nav_feed_tap")
+          private var data: EventData {
+            switch self {
 
-          case .trackCpPageView(let course_id, let course_name):
-        return EventData(name:"cp_page_view", properties: ["course_id": course_id, "course_name": course_name])
+            case .trackNavFeedTap
+              return EventData(name:"nav_feed_tap")
 
+            case .trackCpPageView(let course_id, let course_name):
+              return EventData(name:"cp_page_view", properties: ["course_id": course_id, "course_name": course_name])
+          }
         }
-      }
       Swift
     }
     it { should eq trim_spaces(expected) }
   end
 
-  describe '#swift_case' do
-    context 'event has no properties' do
-      event = Evva::MixpanelEvent.new('nav_feed_tap', [])
-      it 'should create a case for an event to be tracked' do
-        expected = "\t\tcase trackNavFeedTap\n"
-        expect(generator.swift_case(event)).to eq expected
-      end
-    end
-
-    context 'event has properties' do
-      event = Evva::MixpanelEvent.new('cp_page_view', 'course_id:Long,course_name:String')
-      it 'should create a case for an event to be tracked' do
-        expected = "\t\tcase trackCpPageView(course_id:Long,course_name:String)\n"
-        expect(generator.swift_case(event)).to eq expected
-      end
-    end
-  end
-
   describe '#process_arguments' do
     context "with optional arguments" do
-      properties = 'course_id:Long,course_name:String,from_screen: CourseProfileSource?'
+      let(:properties) { 'course_id:Long,course_name:String,from_screen: CourseProfileSource?' }
+
       it 'processes the arguments looking for special properties' do
         expected = '"course_id": course_id, "course_name": course_name, "from_screen": from_screen.rawValue'
         expect(generator.process_arguments(properties)).to eq expected
@@ -66,7 +49,8 @@ describe Evva::SwiftGenerator do
     end
 
     context "without optional arguments" do
-      properties = 'course_id:Long,course_name:String,from_screen: CourseProfileSource'
+      let(:properties) { 'course_id:Long,course_name:String,from_screen: CourseProfileSource' }
+
       it 'processes the arguments looking for special properties' do
         expected = '"course_id": course_id, "course_name": course_name, "from_screen": from_screen.rawValue'
         expect(generator.process_arguments(properties)).to eq expected
@@ -81,11 +65,12 @@ describe Evva::SwiftGenerator do
         import Foundation
 
         enum CourseProfileSource: String {
-        case course_discovery = "course_discovery"
-        case synced_courses = "synced_courses"
+          case course_discovery = "course_discovery"
+          case synced_courses = "synced_courses"
         }
       Swift
      }
+
     it { should eq trim_spaces(expected) }
   end
 
@@ -103,6 +88,7 @@ describe Evva::SwiftGenerator do
       }
       Swift
     }
+
     it { should eq trim_spaces(expected) }
   end
 end
